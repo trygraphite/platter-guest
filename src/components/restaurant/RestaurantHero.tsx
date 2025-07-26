edcd7960002770fe9d03470af94754fa5a1f884a
IbrahimDoba <ibrahimdoba55@gmail.com>
2025-07-26 18:18:04 +0100

    remove(dark-spots)

--- src/components/restaurant/RestaurantHero.tsx
+++ src/components/restaurant/RestaurantHero.tsx
@@ -105,10 +105,6 @@ export const RestaurantHero = ({ data, onCtaClick, isLoading = false }: Restaura
         </div>
       </div>
       
-      {/* Decorative Elements */}
-      <div className="absolute top-10 left-10 w-20 h-20 bg-primary/10 rounded-full blur-xl animate-pulse" />
-      <div className="absolute bottom-10 right-10 w-32 h-32 bg-primary/5 rounded-full blur-2xl animate-pulse" style={{ animationDelay: '1s' }} />
-      
       {/* Hours Modal */}
       <HoursModal
         isOpen={isHoursModalOpen}
